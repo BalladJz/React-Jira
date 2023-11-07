ea38e0e599e8b187faf66f569a5e87e49e4c4b12
Ballad <751464436@qq.com>
2023-11-07 08:11:53 +0800

    feat: custom-hooks

--- src/views/projectList/index.jsx
+++ src/views/projectList/index.jsx
@@ -4,7 +4,7 @@ import qs from "qs";
 
 import { List } from "./List";
 import { SearchPanel } from "./SearchPanel";
-import { cleanObject } from "../utils";
+import { cleanObject, useMount, useDebounce } from "../utils";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -14,38 +14,33 @@ export const ProjectList = () => {
     name: "",
     personId: "",
   });
+  const debounceParam = useDebounce(param, 2000);
   // 下拉框选择的选项
   const [users, setUsers] = useState([]);
   // 渲染的列表
   const [list, setList] = useState([]);
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
-      async (res) => {
-        if (res.ok) {
-          setList(await res.json());
-        }
-      },
-    );
-  }, [param]);
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`,
+    ).then(async (res) => {
+      if (res.ok) {
+        setList(await res.json());
+      }
+    });
+  }, [debounceParam]);
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (res) => {
       if (res.ok) {
         setUsers(await res.json());
       }
     });
-    // ! 当组件初始化时，会执行一次
-  }, []);
+  });
 
   return (
     <div>
-      <SearchPanel
-        param={param}
-        setParam={setParam}
-        users={users}
-        setUsers={setUsers}
-      />
+      <SearchPanel param={param} setParam={setParam} users={users} />
       <List users={users} list={list} />
     </div>
   );
--- src/views/utils/index.js
+++ src/views/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 // 对参数为零进行处理
 export const isFalsy = (value) => (value === 0 ? true : !!value);
 
@@ -11,4 +13,31 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+
+/** 对 useEffect初始化时进行封装 */
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+/** 封装输入框的防抖 */
+export const useDebounce = (value, delay) => {
+  // 准备一个debounce变量
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  // 每次value变化时， 调用 useDebounce 通过 useEffect  触发
+  useEffect(() => {
+    // 设置一个定时器
+    const timeout = setTimeout(() => {
+      setDebounceValue(value);
+    }, delay);
+
+    // 每次在上一个useEffect 处理完以后在运行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  // 最后返回出内部的 debounceValue
+  return debounceValue;
 };
