0fbc895d981c252833a7b463c616ca6a69b0d623
Ballad <751464436@qq.com>
2023-11-08 20:47:19 +0800

    feat: 利用React表单实现、TS集成类型实现登录表单

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { ProjectList } from "views/projectList/index";
 import TryUseArray from "views/tryUseArray/index";
+import LoginPage from "views/login/index";
 
 function App() {
   return (
     <div className="App">
       {/* <ProjectList /> */}
-      <TryUseArray />
+      {/* <TryUseArray /> */}
+      <LoginPage />
     </div>
   );
 }
